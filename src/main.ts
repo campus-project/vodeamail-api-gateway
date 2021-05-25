162516dd9a1231952cfead7e404b0b73204b84fd
tanyudii <y.hertanto17@gmail.com>
2021-05-26 02:05:48 +0700

    feat: 🎸 adjustment campaign

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import { ConfigService } from '@nestjs/config';
 
@@ -8,19 +8,17 @@ import { ConfigService } from '@nestjs/config';
   const configService = new ConfigService();
   const app = await NestFactory.create(AppModule);
 
-  // //is used for transform pipes message
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     transform: true,
-  //     whitelist: true,
-  //   }),
-  // );
-  //
-  // //is used for exclude attribute in entity
-  // app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  //
-  // //is used for allow custom pipes attribute
-  // useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  //is used for transform pipes message
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      stopAtFirstError: true,
+    }),
+  );
+
+  //is used for allow custom pipes attribute
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   await app.listen(configService.get<number>('APP_PORT') || 3000);
 })();
