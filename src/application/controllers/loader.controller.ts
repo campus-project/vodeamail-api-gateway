7a3698f5586a5cd36b47eb310c22d577be922588
Yudi Hertanto <39409106+tanyudii@users.noreply.github.com>
2021-07-25 14:13:47 +0700

    Update loader.controller.ts
    add public

--- src/application/controllers/loader.controller.ts
+++ src/application/controllers/loader.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from '../../@core/decorators/is-public.decorator';
 
 @Controller()
 export class LoaderController {
+  @Public()
   @Get('loaderio-5f6a5bed467e7d14aa3ebc295f7589eb')
   loaderTest() {
     return 'loaderio-5f6a5bed467e7d14aa3ebc295f7589eb';
